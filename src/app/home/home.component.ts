696aef73b1ad0123c2bf4c17da3cccd0a561b77f
Damika.Alwis <Damika.Alwis@cloudsolutions.com.sa>
2026-05-01 18:40:39 +0530

    feat: implement showLoaderUntilCompleted function in LoadingService to manage global loading states during course data fetching

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,15 +27,14 @@ export class HomeComponent implements OnInit {
   }
 
   reloadCourses(){
-    this.loadingService.loadingOn();
     const courses$ = this.CoursesService.getCourses().pipe(
-      map(courses=> courses.sort(sortCoursesBySeqNo)),
-      finalize(()=> this.loadingService.loadingOff())
-    );
-    this.beginnerCourses$ = courses$.pipe(
+      map(courses=> courses.sort(sortCoursesBySeqNo))
+      );
+    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
+    this.beginnerCourses$ = loadCourses$.pipe(
       map(courses => courses.filter(course => course.category == "BEGINNER"))
     );
-    this.advancedCourses$ = courses$.pipe(
+    this.advancedCourses$ = loadCourses$.pipe(
       map(courses => courses.filter(course => course.category == "ADVANCED"))
     );
   }
--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
+import { of } from "rxjs/internal/observable/of";
+import { tap, finalize, concatMap } from "rxjs/operators";
 
 @Injectable()
 export class LoadingService{
     private loadingSubject : BehaviorSubject<boolean> = new BehaviorSubject(false);
     loading$ : Observable<boolean> = this.loadingSubject.asObservable();
-    // showLoaderUntilCompleted
+
+    showLoaderUntilCompleted<T>(ob$ : Observable<T>) : Observable<T>{
+        return of(null)
+        .pipe(
+            tap(() => this.loadingOn()),
+            concatMap(() => ob$),
+            finalize(() => this.loadingOff())
+        );
+    }
+
     loadingOn(){
         this.loadingSubject.next(true);
     }
+
     loadingOff(){
         this.loadingSubject.next(false);
     }
