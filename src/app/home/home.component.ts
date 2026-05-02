cb51617274f96836d743533fbe83a457fdc10143
Damika.Alwis <Damika.Alwis@cloudsolutions.com.sa>
2026-05-02 23:23:18 +0530

    feat: implement reactive course data fetching with shared error handling and messaging services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
 import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
 import { CoursesService } from '../services/coursers.service';
 import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
 
 
 @Component({
@@ -19,7 +20,7 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private CoursesService: CoursesService, private loadingService: LoadingService) {
+  constructor(private CoursesService: CoursesService, private loadingService: LoadingService, private messagesService: MessagesService) {
   }
 
   ngOnInit() {
@@ -28,7 +29,12 @@ export class HomeComponent implements OnInit {
 
   reloadCourses(){
     const courses$ = this.CoursesService.getCourses().pipe(
-      map(courses=> courses.sort(sortCoursesBySeqNo))
+      map(courses=> courses.sort(sortCoursesBySeqNo)),
+      catchError(err => {
+        this.messagesService.showMessages(err?.message || 'Could not load courses');
+        return of([]);
+        // return throwError(()=> err);
+      })
       );
     const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
     this.beginnerCourses$ = loadCourses$.pipe(
